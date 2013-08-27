eafdf3e8519fc60618f84113e8d5b09af3239098
andreiolaru <cs@andreiolaru.ro>
2013-08-27 19:55:27 +0300

    Added some first classes.

---
+++ src/net/xqhs/atasker/core/Task.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2013 Andrei Olaru.
+ * 
+ * This file is part of aTasker.
+ * 
+ * aTasker is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * 
+ * aTasker is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with aTasker.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package net.xqhs.atasker.core;
+
+import net.xqhs.graphs.graph.ConnectedNode;
+
+public class Task
+{
+	ElasticityModel	elModel;
+	Interval		time;
+	ConnectedNode	graphNode;
+}
---
+++ src/net/xqhs/atasker/core/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Core classes and interfaces for tasks.
+ */
+/**
+ * @author Andrei Olaru
+ *
+ */
+package net.xqhs.atasker.core;
